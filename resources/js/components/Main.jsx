ace187ec731f0bb7a06bf442a5b63a4aa07c012f
Charly1346 <arturoglez7368@gmail.com>
2024-04-24 05:15:13 -0600

    Primer avance frontend y Validaciones para login

--- resources/js/components/Main.jsx
+++ resources/js/components/Main.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import Menu from "./Menu";
 import Example from "./Example";
 import { Routes, Route, Navigate } from 'react-router-dom';
+import LoginForm from './Login';
 
-import AuthPage from './AuthPage';
+//import AuthPage from './AuthPage';
 
-function Main (){
-    return(        
-        <Routes>
-        <Route path="/INVITATIONS/example-app/public/"
-                  element={<Menu />}>
-            {/* <Route index element={<Home /> } /> */}
-            <Route path="listcard" element={<AuthPage /> } />
-            
-            
-            
-          <Route path="*" 
-              element={<Navigate replace to="/INVITATIONS/example-app/public" />} />
-        </Route>         
-             
-      </Routes>
+function Main() {
+  return (
+    <Routes>
+      <Route path="/Invitations/public/"
+        element={<Menu />}>
+        {/* <Route index element={<Home /> } /> */}
+        <Route path="listcard" element={<LoginForm />} />
 
-        
-    )
+
+        <Route path="*"
+          element={<Navigate replace to="/Invitations/public/" />} />
+      </Route>
+
+
+
+    </Routes>
+
+
+  )
 }
 
 export default Main;
